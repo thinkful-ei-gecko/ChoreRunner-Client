09b64352415c8013dbd4a9a82527e3f00c8ad000
psychicbologna <alexander.fukui@gmail.com>
2019-11-25 14:46:56 -0600

    fixed config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ export default {
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:8000/api'
       : 'ENTER LIVE ZEIT URL HERE',
-  TOKEN_KEY: 'bdae8b61-2eb5-4ec2-aefe-8042fedd92f9',
+  TOKEN_KEY: process.env.REACT_APP_API_KEY,
 }
