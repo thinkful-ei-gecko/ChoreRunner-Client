409a1bdb4bd3c75adf19a64fae0f156fc7b8e468
Yulia Khisamutdinova <yulia-kh@users.noreply.github.com>
2019-11-26 09:30:56 -0800

    wip parent dashboard

--- src/components/ParentDashboard/ParentDashboard.js
+++ src/components/ParentDashboard/ParentDashboard.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import ApiService from '../../services/api-service'
 
 export default class ParentDashboard extends Component {
+    state = {
+      household:'',
+    }
+
+    handleHouseholdSubmit = e => {
+        e.preventDefault();
+        let name=e.target.householdName.value
+        ApiService.postHousehold(name)
+          .then(result => {
+              console.log('handle submit res', result)
+              this.setState({
+                  household: result
+              })
+          })
+    }
+
     render() {
+        console.log(this.state.household)
         return (
             <div>
                 <h2>PARENT DASHBOARD</h2>
                 <div>
-                    ADD HOUSEHOLD FORMS HERE
-                    <p> example: add household</p>
-                    <p> example: add household members</p>
+                 <form onSubmit={this.handleHouseholdSubmit}>
+                     <label htmlFor="household-name"></label>
+                     <input type="text" id="household-name" name="householdName"></input>
+                     <button type="submit">Sumbit</button>
+                 </form>
                 </div>
                 <div className='household-details container'>
                     ----------------------- HOUSEHOLD DETAILS ----------------------
