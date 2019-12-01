e6b50ed60044618d4535d898238e9c043cbc6261
nphiukhao <nphiukhao@gmail.com>
2019-12-01 15:07:03 -0800

    parent dashboard styled

--- src/components/AddMembers/AddMembers.js
+++ src/components/AddMembers/AddMembers.js
@@ -23,15 +23,15 @@ export default class AddMembers extends React.Component {
     fetch(`${config.API_ENDPOINT}/households/${this.state.household_id}/members`, {
       method: 'POST',
       headers: {
-          'content-type': 'application/json',
-          'authorization': `bearer ${TokenService.getAuthToken()}`
+        'content-type': 'application/json',
+        'authorization': `bearer ${TokenService.getAuthToken()}`
       },
       body: JSON.stringify(newMember)
-  })
+    })
       .then(res =>
-          (!res.ok)
-              ? res.json().then(e => Promise.reject(e))
-              : res.json()
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
       )
       .then(member => console.log('in addmember', member))
   }
@@ -61,27 +61,27 @@ export default class AddMembers extends React.Component {
   }
 
   render() {
-    const {households} = this.context
+    const { households } = this.context
     console.log(households);
-    return(
-  
-        <div className="add-household container">
-        <h2>Add household members</h2>
-          <form onSubmit={this.handleSubmit}  className="add-household-form">
-            <label htmlFor="member-name">Name</label>
-            <input type="text" id="member-name" required onChange={this.handleNameChange}></input>
-            <label htmlFor="household">Household</label>
-            <select type="text" id="assignee" required onChange={this.handleHouseholdChange}>
-              {households.map((hh, index) => <option key={index} value={hh.id}>{hh.name}</option>)}
-            </select>
-            <label htmlFor="child-username">Child username</label>
-            <input type="text" id="child-username" required onChange={this.handleChildUsernameChange}></input>
-            <label htmlFor="child-password">Child password</label>
-            <input type="password" id="child-password" required onChange={this.handleChildPasswordChange}></input>
-            <button type="submit" className="submitHH">add</button>
-          </form>
-        </div>
-    
+    return (
+
+      <div className="add-member container">
+        <p>ADD HOUSEHOLD MEMBERS:</p>
+        <form onSubmit={this.handleSubmit} className="add-household-form">
+          <label htmlFor="member-name">Name</label>
+          <input type="text" id="member-name" required onChange={this.handleNameChange}></input>
+          <label htmlFor="household">Household</label>
+          <select type="text" id="assignee" required onChange={this.handleHouseholdChange}>
+            {households.map((hh, index) => <option key={index} value={hh.id}>{hh.name}</option>)}
+          </select>
+          <label htmlFor="child-username">Child username</label>
+          <input type="text" id="child-username" required onChange={this.handleChildUsernameChange}></input>
+          <label htmlFor="child-password">Child password</label>
+          <input type="password" id="child-password" required onChange={this.handleChildPasswordChange}></input>
+          <button type="submit" className="submitHH">add</button>
+        </form>
+      </div>
+
     )
   }
 }
