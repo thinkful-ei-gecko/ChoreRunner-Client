7f868d3d16aa9f6a8ec9d6c8bae9b20adf8a6489
Daniel Nichols <52935999+DanielCNichols@users.noreply.github.com>
2019-12-10 17:09:37 -0500

    some styling

--- src/components/Badge/Badge.js
+++ src/components/Badge/Badge.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import ApiService from '../../services/api-service';
+import './Badge.css';
+// import { Doughnut } from 'react-chartjs-2';
 //THIS MIGHT NOT BE NECESSARY, DEPENDING ON OUR REFACTORING OF ENDPOINTS.
 //AT THE VERY LEAST, THE REQUESTED ENDPOINT WILL NEED TO BE CHANGED.
 
@@ -8,7 +10,6 @@ export default class Badge extends Component {
     levelInfo: {},
   };
 
-  
   componentDidMount() {
     ApiService.getBadge().then(res => {
       this.setState({ levelInfo: res });
@@ -16,13 +17,48 @@ export default class Badge extends Component {
   }
 
   render() {
-    const {levelInfo} = this.state
+    const { levelInfo } = this.state;
+    const nextLevelPoints =
+      levelInfo.level_id === 1
+        ? levelInfo.level_id * 10
+        : (levelInfo.level_id + 1) * 10;
+
+    // this.options = {
+    //   responsive: true,
+    //   legend: {
+    //     display: false,
+    //   },
+    //   title: {
+    //     display: false,
+    //     text: 'Chart.js Doughnut Chart',
+    //   },
+    // };
+
+    // this.data = {
+    //   labels: ['Current Score', 'To Next Level'],
+    //   datasets: [
+    //     {
+    //       data: [levelInfo.total_score, nextLevelPoints],
+    //       backgroundColor: ['#00A550'],
+    //     },
+    //   ],
+    // };
+
     return (
       <section className="levelInfo">
-        <div className="badge">{levelInfo.badge}</div>
-        <div className="level">Level: {levelInfo.level_id}</div>
-        <div className="totalScore">Total Score: {levelInfo.total_score}</div>
-        <div className="NextLevel">To next level: {levelInfo.nextLevel}</div>
+        <div className="badge">
+          <div className="image">{levelInfo.badge}</div>
+          {/* <Doughnut options={this.options} data={this.data} /> */}
+        </div>
+        <div className="level">
+          <div className="level_totalScore">
+            <h3>Total Score</h3>
+            <span>{levelInfo.total_score}</span>
+          </div>
+          <div className="level_NextLevel">
+            To next level: {levelInfo.nextLevel}
+          </div>
+        </div>
       </section>
     );
   }
