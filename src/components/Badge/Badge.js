8e9518cdebfe8b47a3a2c3921a8a6012304f010a
Daniel Nichols <52935999+DanielCNichols@users.noreply.github.com>
2019-12-11 10:10:00 -0500

    updated some styles on the leaderboard. Not finished

--- src/components/Badge/Badge.js
+++ src/components/Badge/Badge.js
@@ -17,54 +17,35 @@ export default class Badge extends Component {
     });
   }
 
-  // return (
-  //   <section className="levelInfo">
-  //     <div className="badge">
-  //       <div className="image">{levelInfo.badge}</div>
-  //       {/* <Doughnut options={this.options} data={this.data} /> */}
-  //     </div>
-  //     <div className="level">
-  //       <div className="level_totalScore">
-  //         <h3>Total Score</h3>
-  //         <span>{levelInfo.total_score}</span>
-  //       </div>
-  //       <div className="level_NextLevel">
-  //         To next level: {levelInfo.nextLevel}
-  //       </div>
-  //     </div>
-  //   </section>
-
-  //NANCY
-  findImage = string => {
-    console.log(string); //Badge1
-    const idx = string.indexOf('.'); //There's no period
-    let badge = string.slice(0, idx); // There's no index...
-    console.log(badge); //Returns 'Badge'
-    return badge + 1; //Added the 1 to make it work. But doesn't this end up just being the string that was passed in?
-  };
-  renderBadgeArea = () => {
+  renderBadge() {
     const { levelInfo } = this.state;
     return (
       <section className="levelInfo">
-        {/* <div className="badge"> */}
-        <img src={images[levelInfo.badge]} alt="level badge" /> //So... could
-        just feed in the value from the DB and call it done?
-        {/* </div> */}
-        <div className="level">Level: {levelInfo.level_id}</div>
-        <div className="totalScore">Total Score: {levelInfo.total_score}</div>
-        <div className="NextLevel">To next level: {levelInfo.nextLevel}</div>
+        <div className="badge">
+          <div className="image">
+            <img src={images[levelInfo.badge]} alt="Badge png" />
+          </div>
+        </div>
+        <div className="level">
+          <div className="level_totalScore">
+            <h3>Total Score</h3>
+            <p className="total_score">{levelInfo.total_score}</p>
+            {/* </div> */}
+            {/* {/1* <div className="level_NextLevel"> *1/} */}
+            <p>To next level: {levelInfo.nextLevel}</p>
+            {/* </div>{' '} */}
+          </div>
+        </div>
       </section>
     );
-  };
+  }
 
   render() {
     if (this.state.levelInfo.badge) {
-      return this.renderBadgeArea();
+      return this.renderBadge();
     }
     const { levelInfo } = this.state;
     console.log(this.state);
-    return (
-      <>{!!levelInfo.badge ? this.renderBadgeArea() : <p> LOADING... </p>}</>
-    );
+    return <>{!!levelInfo.badge ? this.renderBadge() : <p> LOADING... </p>}</>;
   }
 }
