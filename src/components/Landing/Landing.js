65a904363548dc683572fde4cae5933726a875e6
Daniel Nichols <52935999+DanielCNichols@users.noreply.github.com>
2019-12-12 15:29:52 -0500

    landing page layout. Please check!

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,47 +1,112 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import './Landing.css'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Landing.css';
+import images from '../../ImgAssets/index';
 
 export default class Landing extends Component {
-	render() {
-		return (
-			<div className='landing'>
-				<h1>Chore Runner</h1>
-				<p className="tagline">The Smart Shared To-Do List</p>
-				<div className='header-background'>
-					<h2>Nobody's Perfect</h2>
-				</div>
-	
-				<p>Is it done? Whose turn was it to do the dishes anyway? Didn't you ask her to finish that two days ago? How did you forget this?</p>
+  render() {
+    return (
+      <div className="landing">
+        <h1>Chore Runner</h1>
+        <p className="tagline">The Smart Shared To-Do List</p>
 
-				<p>Chores pile up around you like loud, hungry cats around an empty dish. You can only deal with one thing at a time. So you ask your family to step in. But delegating to them is a thankless, stressful job. There's also that one family member with constant amnesia and the other who remembers too much.</p>
+        <div className="button-container">
+          <Link
+            className="kid-button"
+            tabIndex={1}
+            style={{ textDecoration: 'none' }}
+            to="/kidLogin"
+          >
+            I am a kid
+          </Link>
+          <Link
+            className="parent-button"
+            tabIndex={2}
+            style={{ textDecoration: 'none' }}
+            to="/login"
+          >
+            I am a parent
+          </Link>
+          <Link
+            className="new-button"
+            tabIndex={3}
+            style={{ textDecoration: 'none' }}
+            to="/register"
+          >
+            I am new
+          </Link>
+        </div>
+            <h3>For kids</h3>
+        <div className="kids-container">
+          <div className="badge-placeholder">
+						<img src={images.Badge10} alt="Badge"></img>
+					</div>
+          <div className="kids">
 
-				<p>So you've decided that having a system in place will clear things up. Everyone has to get on board, which also means you have to bribe them. Sounds great, right?</p>
+            <h4>Complete challenges, get badges!</h4>
+            <p>
+              Chores are boring, games are fun! The rules are simple: race your
+              family members to complete chore-lenges, earn points, and get
+              badges. Can you collect all 10 badges and become the top
+              chorerunner in your house? Ask your parents to help you sign up
+              today!
+            </p>
 
-				<p>ChoreRunner can help here. This app keeps track of the status of any given task and assigns it to whoever you want. It also creates incentives for the more reluctant members of your family by making it into a game.</p>
+						<Link
+            className="kid-button"
+            tabIndex={4}
+            style={{ textDecoration: 'none' }}
+            to="/kidLogin"
+          >
+            I am a kid
+          </Link>
+          </div>
+        </div>
+            <h3>For parents</h3>
+        <div className="parent-container">
+          <div className="parents">
 
-				<h2>How it Works</h2>
-
-				<p>To get started, you sign up and set your households.  Your minions then create their accounts. Each member of the family can be assigned jobs. When they finish, they check it off themselves. If it's up to your standards, you confirm the item is complete.</p>
-
-				<p>Here comes the fun part! ChoreRunner has a reward system in place for completing chores. You can assign point values to tasks. For example, doing the dishes could be worth 5 points, mowing the lawn worth 10. Your family members can level up with enough points. A ranking board lets everyone see who is the most on top of their chores.</p>
-
-				<p>ChoreRunner will organize that busy to-do list into something a little more manageable. Everyone wants to help, so help them show you how!</p>
-
-				<div className='img-placeholder'>IMAGE/VIDEO PLACEHOLDER</div>
-				<div className='img-placeholder'> IMAGE/VIDEO PLACEHOLDER</div>
-
-				<div className='button-container'>
-					<Link className='kid-button' tabIndex={1} style={{ textDecoration: 'none' }} to='/kidLogin'>I am a kid</Link>
-					<Link className='parent-button' tabIndex={2} style={{ textDecoration: 'none' }} to='/login'>I am a parent</Link>
-					<Link className='new-button' tabIndex={3} style={{ textDecoration: 'none' }} to='/register'>I am new</Link>
-				</div>
-
-                {/* <Link tabIndex={1 } style={{ textDecoration: 'none' }} to='/kidLogin'>I am a kid</Link>
-                <Link tabIndex={2 } style={{ textDecoration: 'none' }} to='/login'>I am a parent</Link>
-                <Link tabIndex={3 } style={{ textDecoration: 'none' }} to='/register'>I am new</Link> */}
-
-			</div>
-		)
-	}
+            <h4>Make chores easy and fun!</h4>
+            <p>
+              Who's turn is it to do the dishes? Didn't you ask them to finish
+              this two days ago?
+            </p>
+            <p>
+              Sound familiar? We know that managing and delegating household
+              chores can be a struggle. Let Chorerunner help!
+            </p>
+            <p>
+              Chorerunner makes chores a fun competition between kids and family
+              members. Simply sign up, add household members, assign tasks, and
+              let chorerunner do the rest. As members complete tasks, they move
+              up the household leaderboard, gain levels, and earn badges.
+            </p>
+            <p>
+              Spend less time working and more time playing. Sign in or sign up to get
+              started!
+            </p>
+						<Link
+            className="parent-button"
+            tabIndex={2}
+            style={{ textDecoration: 'none' }}
+            to="/login"
+          >
+            I have and account
+          </Link>
+					<Link
+            className="new-button"
+            tabIndex={3}
+            style={{ textDecoration: 'none' }}
+            to="/register"
+          >
+            I am new
+          </Link>
+          </div>
+          <div className="img-placeholder">
+					<iframe title="test-video" width="auto" height="300" src="https://www.youtube.com/embed/McMzoUhhym4" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+					</div>
+        </div>
+      </div>
+    );
+  }
 }
