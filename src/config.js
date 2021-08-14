0253c98cbc0addb92d4148a1a2b1810b848345ca
Hubert Yang <offspring7890@yahoo.com>
2021-08-14 09:38:34 -0700

    done

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://peaceful-wave-32071.herokuapp.com/',
   TOKEN_KEY: 'chorerunner-jwt-secret', 
   TYPE: 'type'
 }
